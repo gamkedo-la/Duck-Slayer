6785ee3e8d9ff8c2e5dbc448ea21257b90bc263d
Workbook0 <workbook0@Workbook0s-MacBook-Pro.local>
2021-02-04 12:40:46 -0800

    adds mouselook using old Input System and adds sample files for new Input System

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -1,31 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensativity = 100f;
+    
     public Transform playerBody;
+    public float mouseSensitivity = 100f;
     float xRotation = 0f;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-       Cursor.lockState = CursorLockMode.Locked; 
+      Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+      float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+      float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+      xRotation -= mouseY;
+      xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); 
-        
-        playerBody.Rotate(Vector3.up * mouseX);
+
+      transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+      playerBody.Rotate(new Vector3(0, 1, 0) * mouseX);
+
+      Debug.Log(playerBody.rotation);
     }
 
     
