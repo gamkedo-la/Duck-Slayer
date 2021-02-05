cc3708edf8fc4862c713da444ba63c6350102b26
Ryan Cmahela <6194542+RyanCahela@users.noreply.github.com>
2021-02-05 09:59:15 -0800

    adds gun and target raycast functionality

---
+++ Assets/GunFire.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    public Transform fpsCam;
+
+    float damage = 100f;
+    float range = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetButtonDown("Fire1")) {
+        Shoot();
+      }
+    }
+
+    void Shoot() 
+    {
+      RaycastHit hit;
+      if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+      {
+        Target target = hit.transform.GetComponent<Target>();
+        if(target != null) {
+          target.TakeDamage(damage);
+        }
+      }
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+  float health = 200f;
+  public void TakeDamage(float amount)
+  {
+    health -= amount;
+    if(health <= 0) {
+      Die();
+    }
+  }
+
+  void Die() {
+    Destroy(gameObject);
+  }
+}
+
