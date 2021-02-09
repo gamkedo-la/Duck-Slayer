48c51707eb6e13d484c3c51d1da8cd7e7bcc4617
Ryan Cmahela <6194542+RyanCahela@users.noreply.github.com>
2021-02-09 14:34:40 -0800

    adds gun shot visualization and raycast debug visualizer

--- Assets/Scripts/GunFire.cs
+++ Assets/Scripts/GunFire.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GunFire : MonoBehaviour
 {
-    public Transform fpsCam;
+    public Transform gunEnd;
+    public float damage = 100f;
+    public float range = 1000f;
+    public float fireRate = 0.25f;
 
-    float damage = 100f;
-    float range = 1000f;
-    // Start is called before the first frame update
+    private LineRenderer laserLine;
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.1f);
+    private float nextFireTime;
+
     void Start()
     {
-        
+      laserLine = GetComponent<LineRenderer>();
+      fpsCam = GetComponentInParent<Camera>();
     }
 
     // Update is called once per frame
@@ -23,13 +30,35 @@ public class GunFire : MonoBehaviour
     void Shoot() 
     {
       RaycastHit hit;
-      if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+      Vector3 endOfLaser;
+      Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+      StartCoroutine(ShotEffect());
+      laserLine.SetPosition(0, gunEnd.position);
+
+      if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
       {
-        
+        //handle damage if hit
         Target target = hit.transform.GetComponent<Target>();
         if(target != null) {
           target.TakeDamage(damage);
         }
+
+        //handle laser render on hit
+        endOfLaser = hit.point;
+      } else
+      {
+        //handle laser render on no hit
+        endOfLaser = rayOrigin + (fpsCam.transform.forward * range);
       }
+
+      laserLine.SetPosition(1, endOfLaser);
+    }
+
+    private IEnumerator ShotEffect()
+    {
+      laserLine.enabled = true;
+      yield return shotDuration;
+      laserLine.enabled = false;
+
     }
 }
