4a54be406b66abae1c7ec7d1b842951f7f9177d2
Ryan Cmahela <6194542+RyanCahela@users.noreply.github.com>
2021-04-17 11:17:28 -0700

    added VR/WebGL Detection

---
+++ Assets/Scripts/SetVROrNot.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVROrNot : MonoBehaviour {
+    public bool forceVR = false;
+
+    public GameObject webController;
+    public GameObject vrController;
+    public GameObject postProcessingVolume;
+    public bool disablePostProcessinginVR = false;
+
+    public Canvas eventCamToChange;
+    public Camera eventCamNonVR;
+    public Camera eventCamIfVR;
+
+    void Awake() {
+        if (Application.platform == RuntimePlatform.WebGLPlayer) {
+            forceVR = false;
+        } else if (Application.platform == RuntimePlatform.Android) {
+            forceVR = true;
+        }
+
+        if(eventCamToChange != null) {
+            if(forceVR) {
+                eventCamToChange.worldCamera = eventCamIfVR;
+            } else {
+                eventCamToChange.worldCamera = eventCamNonVR;
+            }
+        }
+
+        vrController.SetActive(forceVR);
+        webController.SetActive(!forceVR);
+
+        if (forceVR && disablePostProcessinginVR)
+        {
+            if (postProcessingVolume != null)
+            {
+                postProcessingVolume.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("To disable post-processing in VR you have to attach it's volume to this script", gameObject);
+            }
+        }
+    }
+}
