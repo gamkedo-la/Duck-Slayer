e7b63018e8ddd7d6328740d5768bebe64c264f25
rocleemusic <48lee@cardinalmail.cua.edu>
2021-04-21 10:43:07 -0400

    Bezier Curve

--- Assets/Scripts/Editor/BezierCurveInspector.cs
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -10,7 +10,8 @@ public class BezierCurveInspector : Editor
     private Transform handleTransform;
     private Quaternion handleRotation;
 
-    [SerializeField] int lineSteps = 10;
+    private const int lineSteps = 10;
+    private const float directionScale = 2f;
 
     private void OnSceneGUI()
     {
@@ -21,18 +22,30 @@ public class BezierCurveInspector : Editor
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+        //Handles.DrawLine(p1, p2);
+        Handles.DrawLine(p2, p3);
 
-        Handles.color = Color.white;
-        Vector3 lineStart = curve.GetPoint(0f);
+        //Handles.color = Color.white;
+        //Vector3 lineStart = curve.GetPoint(0f);
+
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+    }
+
+    private void ShowDirections()
+    {
+        Handles.color = Color.green;
+        Vector3 point = curve.GetPoint(0f);
+        Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
+
         for (int i = 1; i <= lineSteps; ++i)
         {
-            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-            Handles.DrawLine(lineStart, lineEnd);
-            lineStart = lineEnd;
+            point = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
         }
     }
 
