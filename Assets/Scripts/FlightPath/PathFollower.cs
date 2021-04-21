82e7f6cad9ecdd41e6b05f0934ef910d4dc9e585
rocleemusic <48lee@cardinalmail.cua.edu>
2021-04-21 17:24:04 -0400

    Can make objects follow a spline

--- Assets/Scripts/FlightPath/PathFollower.cs
+++ Assets/Scripts/FlightPath/PathFollower.cs
@@ -2,21 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum PathFollowMode { Once, Loop, PingPong }
 public class PathFollower : MonoBehaviour
 {
     public BezierSpline spline;
+    public PathFollowMode mode;
+    private bool goingForward = true;
 
+    public bool lookForward;
     public float duration;
 
     private float progress;
 
     private void Update()
     {
-        progress += Time.deltaTime / duration;
-        if (progress > 1f)
+        if (goingForward)
         {
-            progress = 1f;
+
+
+            progress += Time.deltaTime / duration;
+            if (progress > 1f)
+            {
+                switch (mode)
+                {
+                    case PathFollowMode.Once:
+                        progress = 1f;
+                        break;
+
+                    case PathFollowMode.Loop:
+                        progress -= 1f;
+                        break;
+
+                    case PathFollowMode.PingPong:
+                        progress = 2f - progress;
+                        goingForward = false;
+                        break;
+                }
+
+            }
         }
-        transform.localPosition = spline.GetPoint(progress);
+        else
+        {
+            progress -= Time.deltaTime / duration;
+            if (progress < 0f)
+            {
+                progress = -progress;
+                goingForward = true;
+            }
+        }
+
+        Vector3 position = spline.GetPoint(progress);
+
+        //transform.localPosition = position;
+        transform.position = position;
+
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(progress));
+        }
     }
 }
