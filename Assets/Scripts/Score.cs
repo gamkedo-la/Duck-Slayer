c48aead27577261c46b4a1d86c1e59ff107e57dc
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2021-04-23 15:43:04 -0700

    Show player score on the sign

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     private int score;
+    public TextMeshPro Display;
 
     public void IncreaseScore()
 	{
 		score++;
 		Debug.Log("Your score is " + score);
+
+        Display.text = "Score: " + score;
 	}
 }
