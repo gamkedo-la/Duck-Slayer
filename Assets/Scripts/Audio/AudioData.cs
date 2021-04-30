7ffd04e8fd144f808b125c15a3ff1703fc2ff21c
rocleemusic <48lee@cardinalmail.cua.edu>
2021-04-30 13:03:31 -0400

    Duck quacks at intervals

---
+++ Assets/Scripts/Audio/AudioData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu(menuName = "DuckSlayer/AudioData", fileName = "New AudioData.asset")]
+public class AudioData : ScriptableObject
+{
+    [SerializeField] List<AudioClip> Sounds = new List<AudioClip>();
+
+    [SerializeField] AudioMixer mixer;
+
+    [SerializeField] bool Loop = false;
+
+    [Range(-80, 0.0001f)]
+    [SerializeField] float Volume;
+
+    [Range(-24, 24)]
+    [SerializeField] float Pitch = 0f;
+
+    [Range(0f, 1f)]
+    [SerializeField] float SpatialBlend = 1f;
+
+    [Range(0, 5)]
+    [SerializeField] float RandomVolume = 0f;
+    [SerializeField] bool RandomizeVolume = false;
+
+    [Range(0, 12)]
+    [SerializeField] float RandomPitch = 0f;
+    [SerializeField] bool RandomizePitch = false;
+
+    #region AudioSource Parameters
+    public float GetVol()
+    {
+        if (RandomizeVolume == true)
+        {
+            return AudioFunctions.DbToLinear(Volume + GetRandomValueOffset(RandomVolume));
+        }
+        else
+        {
+            return AudioFunctions.DbToLinear(Volume);
+        }
+    }
+
+    private float GetRandomValueOffset(float value)
+    {
+        return Random.Range(-value, value);
+    }
+
+    public float GetPitch()
+    {
+        if (RandomizePitch == true)
+        {
+            return AudioFunctions.St2pitch(Pitch + GetRandomValueOffset(RandomPitch));
+        }
+        else
+        {
+            return AudioFunctions.St2pitch(Pitch);
+        }
+    }
+
+    public bool IsLooping() { return Loop; }
+
+    #endregion
+
+    #region AudioClip Logic
+
+    public AudioClip GetClip(GameObject calledBy)
+    {
+        var numberOfClips = Sounds.Count;
+
+        if (numberOfClips == 0)
+        {
+            Debug.LogError("AudioData called by: " + calledBy.name + " does not contain any AudioClips.");
+            return null;
+        }
+
+        if (numberOfClips > 1)
+        {
+            return Sounds[Random.Range(0, numberOfClips)];
+        }
+        else
+        {
+            return Sounds[0];
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Audio/DuckAudio.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource[] audioSources;
+    private int currentSourceIndex = 0;
+    //private int nextSourceIndex;
+    [SerializeField] AudioData audioData;
+    public float minDelayBetweenQuacks;
+    public float maxDelayBetweenQuacks;
+    public bool loopQuacks;
+    private float timeToNextQuack;
+
+    void Awake()
+    {
+
+        InitializeAudioSource(audioSources[currentSourceIndex]);
+    }
+
+    private void Start()
+    {
+        PlayAudio();
+        SetTimer();
+    }
+
+    private void Update()
+    {
+        if (loopQuacks)
+        {
+            timeToNextQuack -= Time.deltaTime;
+
+            if (timeToNextQuack <= 0)
+            {
+                PlayAudio();
+                SetTimer();
+            }
+        }
+
+
+    }
+
+    private void SetTimer()
+    {
+        timeToNextQuack = UnityEngine.Random.Range(minDelayBetweenQuacks, maxDelayBetweenQuacks);
+    }
+
+    private void InitializeAudioSource(AudioSource source)
+    {
+        source.clip = audioData.GetClip(gameObject);
+        source.loop = audioData.IsLooping();
+        source.volume = audioData.GetVol();
+        source.pitch = audioData.GetPitch();
+
+        IncrementIndex();
+    }
+
+    private void IncrementIndex()
+    {
+        currentSourceIndex = (currentSourceIndex++) % (audioSources.Length);
+    }
+
+    public void PlayAudio()
+    {
+        audioSources[currentSourceIndex].Play();
+        IncrementIndex();
+        InitializeAudioSource(audioSources[currentSourceIndex]);
+    }
+
+
+}
