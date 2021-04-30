596df16357895dcd89472b55a52429f41b648bcd
rocleemusic <48lee@cardinalmail.cua.edu>
2021-04-30 14:08:40 -0400

    Basic gunshot sound

---
+++ Assets/Scripts/Audio/MuzzleBlastAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleBlastAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    [SerializeField] AudioData audioData;
+
+    private void Awake()
+    {
+        InitializeAudioSource();
+
+    }
+    private void InitializeAudioSource()
+    {
+        audioSource.clip = audioData.GetClip(gameObject);
+        audioSource.loop = audioData.IsLooping();
+        audioSource.volume = audioData.GetVol();
+        audioSource.pitch = audioData.GetPitch();
+    }
+
+    void Start()
+    {
+        var s = GetComponent<DearVR.DearVRSource>();
+        s.DearVRPlay();
+    }
+}
