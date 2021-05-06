4d62b81226ba62f5c36309969d330d306756b460
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-06 11:21:11 -0400

    debugging death audio

---
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcePlayer : MonoBehaviour
+{
+    [SerializeField] AudioData audioData;
+    [SerializeField] AudioSource audioSource;
+    public bool playOnStart = false;
+
+    void Awake()
+    {
+        InitializeAudioSource(audioSource);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playOnStart)
+            PlayAudio();
+    }
+
+    private void InitializeAudioSource(AudioSource source)
+    {
+        if (source == null) { Debug.LogError("No Audio Source on object: " + gameObject.name); return; }
+
+        source.clip = audioData.GetClip(gameObject);
+        source.loop = audioData.IsLooping();
+        source.volume = audioData.GetVol();
+        source.pitch = audioData.GetPitch();
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.Play();
+        InitializeAudioSource(audioSource);
+    }
+}
