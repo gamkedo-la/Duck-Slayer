982f024a65fef9ab3d6d591595c5a579751134ab
Philip Greene <philip_greene@hotmail.com>
2021-05-08 23:53:50 +0100

    Added start menu and UI interactions

---
+++ Assets/Scripts/UI/UIInteraction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class UIInteraction : MonoBehaviour
+{
+    public InputActionReference FireButton;
+
+    void OnEnable()
+    {
+        FireButton.action.performed += HandleFireButtonPress;
+    }
+
+    void OnDisable()
+    {
+        FireButton.action.performed -= HandleFireButtonPress;
+    }
+
+    void HandleFireButtonPress(InputAction.CallbackContext obj)
+    {        
+        UIFire();
+    }
+
+    private void UIFire()
+    {
+        Debug.Log("Fire");
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 300))
+        {
+            IPointerClickHandler clickHandler = hit.collider.gameObject.GetComponent<IPointerClickHandler>();
+
+            if (clickHandler != null)
+            {
+                Debug.Log("hit " + hit.collider.gameObject.name);
+                PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+                clickHandler.OnPointerClick(pointerEventData);
+            }
+            else
+            {
+                Debug.Log("hit not UI");
+            }
+        }
+    }
+}
