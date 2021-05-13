186a4313da9cd5f94c4312fd8dcbb7e24f47cab0
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-12 23:56:32 -0400

    Create Pool Manager and poolable object classes

---
+++ Assets/Scripts/Audio/ObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+namespace Audio
+{
+    [CreateAssetMenu(menuName = "DuckSlayer/Create Object Pool")]
+    public class ObjectPool : ScriptableObject
+    {
+        [SerializeField] private GameObject poolableObject;
+        [SerializeField] private int poolSize;
+        [SerializeField] private int maxPoolSize;
+        private List<GameObject> pool;
+        private Transform parent;
+
+        public void CreatePool(Transform transform)
+        {
+            parent = transform;
+            pool = new List<GameObject>();
+
+            for (int i = 0; i < poolSize; i++)
+            {
+                var newObject = Instantiate(poolableObject, parent);
+                pool.Add(newObject);
+            }
+        }
+
+        public GameObject GetObject()
+        {
+            if (pool.Count > 0)
+            {
+                var poolObject = pool[0];
+                pool.RemoveAt(0);
+                return poolObject;
+            }
+
+            return Instantiate(poolableObject, parent);
+        }
+
+        public void ReturnObject(GameObject objectToReturn)
+        {
+            if (pool.Count >= maxPoolSize)
+            {
+                Destroy(objectToReturn);
+                return;
+            }
+            
+            pool.Add((objectToReturn));
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/PoolManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Audio
+{
+    public class PoolManager : MonoBehaviour
+    {
+        public static PoolManager instance = null;
+        [SerializeField] private ObjectPool poolType;
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        private void Start()
+        {
+            if (poolType == null)
+            {
+                Debug.LogWarning("No pooltype set", gameObject);
+                return;
+            }
+            
+            poolType.CreatePool(transform);
+        }
+
+        public ObjectPool GetPool() => poolType;
+    }
+}
