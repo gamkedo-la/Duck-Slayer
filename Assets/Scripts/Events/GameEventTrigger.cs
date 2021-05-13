04f6829382a45afb8874bb5f83075eb6a4f75c32
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-12 21:17:30 -0400

    Can trigger restart level

---
+++ Assets/Scripts/Events/GameEventTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Events
+{
+    public class GameEventTrigger : MonoBehaviour
+    {
+        [SerializeField] private GameEvent gameEvent;
+
+        public void TriggerEvent() => gameEvent?.Invoke();
+    }
+}
