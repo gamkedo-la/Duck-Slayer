937cbd7e97592d48e52ee72eb214df8c44d77f8b
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-26 22:08:12 -0400

    setting up LevelWinCondition manager

--- Assets/Scripts/LevelWinCondition.cs
+++ Assets/Scripts/LevelWinCondition.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 public class LevelWinCondition : MonoBehaviour
 {
+    [SerializeField] IntVariable objective;
+    [SerializeField] IntVariable trackedVariable;
     [SerializeField] GameEvent objectiveComplete;
     // Start is called before the first frame update
     void Start()
     {
-
+        if (objective == null)
+        {
+            Debug.LogError("No objective set for this level, Disabling LevelWinCondition!", gameObject);
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (objective.value >= trackedVariable.value)
+            objectiveComplete?.Invoke();
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/IntVariable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DuckSlayer/Create Int Variable")]
+public class IntVariable : ScriptableObject
+{
+    public int value;
+
+    //public static bool operator ==(int a, IntVariable b)
+    //{
+    //    return a == b.value;
+    //}
+
+    //public static bool operator !=(int a, IntVariable b)
+    //{
+    //    return a != b.value;
+    //}
+
+    //public static bool operator >=(int a, IntVariable b)
+    //{
+    //    return a >= b.value;
+    //}
+
+    //public static bool operator <=(int a, IntVariable b)
+    //{
+    //    return a <= b.value;
+    //}
+
+    //public static bool operator ==(IntVariable a, IntVariable b)
+    //{
+    //    return a.value == b.value;
+    //}
+
+    //public static bool operator !=(IntVariable a, IntVariable b)
+    //{
+    //    return a.value != b.value;
+    //}
+
+    //public static bool operator >=(IntVariable a, IntVariable b)
+    //{
+    //    return a.value >= b.value;
+    //}
+
+    //public static bool operator <=(IntVariable a, IntVariable b)
+    //{
+    //    return a.value <= b.value;
+    //}
+}
