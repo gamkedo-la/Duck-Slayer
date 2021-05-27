6b7235bff99ebed947be18f987196d3555df6ad0
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-26 21:49:56 -0400

    Create Temp Win Screen

---
+++ Assets/Scripts/LevelWinCondition.cs
@@ -0,0 +1,20 @@
+﻿using Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelWinCondition : MonoBehaviour
+{
+    [SerializeField] GameEvent objectiveComplete;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
