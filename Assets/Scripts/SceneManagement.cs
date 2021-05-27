0dc2f611c545e3501596c77ddd84d81f79c0ed0f
rocleemusic <48lee@cardinalmail.cua.edu>
2021-05-26 21:38:06 -0400

    Add ability to cycle through scenes for debugging using "J" on keyboard.

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -3,8 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    [SerializeField] int sceneBuildIndex;
+
+    private void Awake()
+    {
+        // if (sceneBuildIndex)
+        sceneBuildIndex = SceneManager.sceneCountInBuildSettings - 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+            LoadNextScene();
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void LoadNextScene()
+    {
+        int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nextIndex > sceneBuildIndex)
+        {
+            Debug.LogWarning("No more scenes!");
+            return;
+        }
+
+        Debug.Log("Loading Scene: " + SceneManager.GetSceneByBuildIndex(nextIndex).name);
+        SceneManager.LoadScene(nextIndex);
     }
 }
