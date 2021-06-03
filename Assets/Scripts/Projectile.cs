303e4663b191e0cb8e349931275e83957393aad5
rocleemusic <48lee@cardinalmail.cua.edu>
2021-06-02 21:16:51 -0400

    if egg spawned below grass level it self destructs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,12 +7,15 @@ public class Projectile : MonoBehaviour
     [SerializeField] TransformRef PlayerPosition;
     [SerializeField] float minSpeed = 1f;
     [SerializeField] float maxSpeed = 5f;
+    [SerializeField] float heightAtWhichWillSelfDestructWhenSpawned = 0.1f;
     private float speed;
     private Vector3 playerPosition;
 
 
     void Start()
     {
+        SelfDestructIfSpawnedTooLow();
+
         playerPosition = PlayerPosition.value.position;
 
         speed = Random.Range(minSpeed, maxSpeed) * Time.deltaTime;
@@ -22,6 +25,12 @@ public class Projectile : MonoBehaviour
             Destroy(this);
             Debug.LogError("No Target, can't shoot", gameObject);
         }
+    }
+
+    private void SelfDestructIfSpawnedTooLow()
+    {
+        if (transform.position.y < heightAtWhichWillSelfDestructWhenSpawned)
+            Destroy(this);
     }
 
     void Update()
