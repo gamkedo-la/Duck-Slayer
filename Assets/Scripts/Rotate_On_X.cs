632681eb2099b5abbdcf4b253faf631350becabc
Philip Greene <philip_greene@hotmail.com>
2021-06-06 14:28:54 +0100

    Created a model for duck egg prefab and added rotation on X axis

---
+++ Assets/Scripts/Rotate_On_X.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_On_X : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotationSpeed, 0f, 0f) * Time.deltaTime);
+    }
+}
