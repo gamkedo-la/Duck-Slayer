85a2fdde7af039ffa3c7358d81c2151755be05fd
rocleemusic <48lee@cardinalmail.cua.edu>
2021-06-16 21:43:42 -0400

    Can reload with Left Trigger Press in VR

---
+++ Assets/Scripts/ReloadButtonXR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Events;
+
+public class ReloadButtonXR : MonoBehaviour
+{
+    public InputActionReference ReloadButton;
+    [SerializeField] GameEvent gameEvent;
+
+    void OnEnable()
+    {
+        ReloadButton.action.performed += HandleReloadButtonPress;
+    }
+
+    void OnDisable()
+    {
+        ReloadButton.action.performed -= HandleReloadButtonPress;
+    }
+
+    void HandleReloadButtonPress(InputAction.CallbackContext obj)
+    {
+        gameEvent?.Invoke();
+    }
+}
