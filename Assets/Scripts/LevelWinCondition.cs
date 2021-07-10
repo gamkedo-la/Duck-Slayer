3a57a8241b577fed1859fd9eda14a0d44a93b5c5
tiagosomda <tsomda@gmail.com>
2021-07-10 12:17:32 -0700

    [bugfix] calling win objective only once

--- Assets/Scripts/LevelWinCondition.cs
+++ Assets/Scripts/LevelWinCondition.cs
@@ -8,6 +8,9 @@ public class LevelWinCondition : MonoBehaviour
     [SerializeField] IntVariable objective;
     [SerializeField] IntVariable trackedVariable;
     [SerializeField] GameEvent objectiveComplete;
+
+    [SerializeField] bool hasCalledObjectiveCompleted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +19,17 @@ public class LevelWinCondition : MonoBehaviour
             Debug.LogError("No objective set for this level, Disabling LevelWinCondition!", gameObject);
             enabled = false;
         }
+
+        hasCalledObjectiveCompleted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (trackedVariable.value >= objective.value)
+        if (hasCalledObjectiveCompleted == false && trackedVariable.value >= objective.value)
+        {
             objectiveComplete?.Invoke();
+            hasCalledObjectiveCompleted = true;
+        }
     }
 }
