4007c23647a41d516f0edd86d67c35cea85e6089
rocleemusic <48lee@cardinalmail.cua.edu>
2021-07-17 19:07:54 -0400

    fix score text backwards issue

--- Assets/Scripts/DistanceResize.cs
+++ Assets/Scripts/DistanceResize.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class DistanceResize : MonoBehaviour
 {
-    private float ratio = 0.3f;
+    [SerializeField] float ratio = 0.3f;
 
     public void Scale(float distance)
     {
-        this.transform.localScale = Vector3.one*ratio*distance;
+        transform.localScale = Vector3.one * ratio * distance;
     }
 }
