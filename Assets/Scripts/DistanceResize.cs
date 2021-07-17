3f79737735ec73633a56c6ca9d015468133d35fa
rocleemusic <48lee@cardinalmail.cua.edu>
2021-07-17 19:24:16 -0400

    adjust text scaling

--- Assets/Scripts/DistanceResize.cs
+++ Assets/Scripts/DistanceResize.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class DistanceResize : MonoBehaviour
 {
     [SerializeField] float ratio = 0.3f;
+    [SerializeField] float maxDistance = 100;
 
     public void Scale(float distance)
     {
-        transform.localScale = Vector3.one * ratio * distance;
+        var textSize = distance > maxDistance ? maxDistance : distance;
+        transform.localScale = Vector3.one * ratio * textSize;
     }
 }
