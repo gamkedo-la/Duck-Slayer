a62bbc7b266491d119ee3479e1b1d5220d322c1e
tiagosomda <tsomda@gmail.com>
2021-07-17 08:26:46 -0700

    animating damage color

---
+++ Assets/Scripts/AnimateDamageScore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnimateDamageScore : MonoBehaviour
+{
+    public TMP_Text label;
+    public Color startColor;
+    public Color endColor;
+
+    void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    public IEnumerator Animate()
+    {
+        Vector3 startPosition = this.transform.position;
+        Vector3 finalPosition = this.transform.position + new Vector3(0,1,0);
+        label.color = startColor;
+        
+        float percentage = 0.0f;
+        while(percentage < 1.0f)
+        {
+            yield return new WaitForEndOfFrame();
+
+            // animate upwards
+            this.transform.position = Vector3.Lerp(startPosition, finalPosition, percentage);
+            percentage += Time.deltaTime;
+
+            // fade label color
+            label.color = Color.Lerp(startColor, endColor, percentage);
+        }
+    }
+}
