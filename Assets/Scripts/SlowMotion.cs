ad658e76fa8de8f74d14a6816ee21cb61f65fd4e
rocleemusic <48lee@cardinalmail.cua.edu>
2021-07-17 20:06:06 -0400

    import playrig from level 1 to level 2

--- Assets/Scripts/SlowMotion.cs
+++ Assets/Scripts/SlowMotion.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class SlowMotion : MonoBehaviour
 {
-  public float slowDownRate;
-	public float slowDownSeconds;
-  private bool isInSlowMo = false;
+    public float slowDownRate;
+    public float slowDownSeconds;
+    private bool isInSlowMo = false;
 
-	private void Update()
-	{
-    if(isInSlowMo && GameManagerSingleton.instance.IsPaused())
+    private void Update()
     {
-      RestoreTimeSpeed();
+        if (isInSlowMo && !GameManagerSingleton.instance.IsPaused())
+        {
+            RestoreTimeSpeed();
+        }
     }
-	}
 
-  private void RestoreTimeSpeed()
-  {
-		Time.timeScale += (1f / slowDownSeconds) * Time.unscaledDeltaTime;
-		Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
-    if(Time.timeScale == 1)
+    private void RestoreTimeSpeed()
     {
-      isInSlowMo = false;
+        Time.timeScale += (1f / slowDownSeconds) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        if (Time.timeScale == 1)
+        {
+            isInSlowMo = false;
+        }
     }
-  }
 
-	public void SlowDown()
-	{
-    isInSlowMo = true;
-		Time.timeScale = slowDownRate;
-		Time.fixedDeltaTime = Time.timeScale * Time.fixedDeltaTime / slowDownRate;
-	}
+    public void SlowDown()
+    {
+        isInSlowMo = true;
+        Time.timeScale = slowDownRate;
+        Time.fixedDeltaTime = Time.timeScale * Time.fixedDeltaTime / slowDownRate;
+    }
 }
